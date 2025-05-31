b26a88ed162cdab322b100c16aa639da50a388e8
Daniel Bajulaiye <danielb@umbc.edu>
2025-05-31 14:45:25 -0400

    Updated Even or Odd Number Guesser with user input loop

---
+++ even_or_odd.py
@@ -0,0 +1,17 @@
+# odd or even number guesser
+
+while True:
+    print("\nWelcome to the odd or even number guesser\nInput your number and the computer guesses if it's odd or even")
+    choice = input("Enter your number (Or type q to quit):")
+
+    if choice.lower() == 'q':
+       print("Thank you for playing")
+       break
+
+    number = int(choice)
+
+    if number % 2  == 0:
+        print("This is an even number")
+    else:
+        print("This is an odd number")
+
